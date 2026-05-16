74c7762ffe22d609771ee603db7e7bf34c278d34
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-05-17 02:15:12 +0530

    Update documentation

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,12 +1,8 @@
 #include "Application.h"
 #include <iostream>
 /*
-    TODO(Fahad):
-    Integrate FSWindow into this for cross-platform linux-window windowing
-    (https://github.com/fahad-cpp/FSWindow)
-
     Last learned:
-    -Linear Images
+    -Image Arrays
 */
 void Application::init() {
 
