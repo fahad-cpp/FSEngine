c7e23de35ae6b8bd44acdf9d2bc03e0a1f2c02c8
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-08-11 20:22:45 +0530

    Proper setup of build system

--- build.bat
+++ build.bat
@@ -1,18 +1,15 @@
 @echo off
 setlocal
 set "EXENAME=VulkanApp.exe"
-set "CONFIG=Debug"
-set "CLEANBUILD=NO"
+set "PRESET=clang-debug"
 
 if not exist build mkdir build >nul 2>nul
-if exist "bin\%EXENAME%" del "bin\%EXENAME%" >nul 2>nul
-
 if not exist CMakeCache.txt set "CLEANBUILD=YES"
 if "%1" == "clean" set "CLEANBUILD=YES"
 
 if "%CLEANBUILD%" == "YES" (
     rmdir /S /Q build
-    cmake --preset default
+    cmake --preset %PRESET%
     if errorlevel 1 (
         echo Cmake Configuration Failed.
         type error.txt
@@ -21,7 +18,7 @@ if "%CLEANBUILD%" == "YES" (
     )
 )
 
-cmake --build build --config "%CONFIG%" --parallel 
+cmake --build --preset %PRESET%
 if errorlevel 1 (
     echo CMake Build Failed.
     type error.txt
